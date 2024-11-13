0f021fb63732d8c53ae77c3094243c2d37e0751b
AYDIN ADNAN <70015348+AydinAdnan@users.noreply.github.com>
2024-11-13 12:50:42 +0530

    Update vite.config.js
    update host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,      // Ensure it's using port 5173 as configured in security groups
+  },
+});
